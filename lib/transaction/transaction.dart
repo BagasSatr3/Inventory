dea71291fed0a868fd7f92462150a7be58c52b5b
FlutLab.io <>
2022-04-30 03:49:19 +0000

    Autosave

--- lib/transaction/transaction.dart
+++ lib/transaction/transaction.dart
@@ -17,129 +17,227 @@ class Main extends StatelessWidget {
           title: Text('Transaction', style: const TextStyle(fontSize: 25, fontWeight: FontWeight.bold)),
         ),
         body: Padding(
-            padding: const EdgeInsets.only(top: 8, left: 8),
-            child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-              Row(
-                children: <Widget>[
-                  Card(
-                    elevation: 2,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(4)),
-                      child: Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Partially-reflected-plutonium-sphere.jpeg/300px-Partially-reflected-plutonium-sphere.jpeg", width: 80, height: 70),
-                    ),
+          padding: const EdgeInsets.only(top: 8, left: 8),
+          child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+            Row(
+              children: <Widget>[
+                Card(
+                  elevation: 2,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(4)),
+                    child: Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Partially-reflected-plutonium-sphere.jpeg/300px-Partially-reflected-plutonium-sphere.jpeg", width: 80, height: 70),
                   ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Demon Core', textAlign: TextAlign.left, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                      RaisedButton(
-                          child: Text('Stok 1'),
-                          color: Colors.red,
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(16.0))),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => TransactionDetail()),
-                            );
-                          }),
-                      Row(children: <Widget>[
-                        Icon(
-                          Icons.arrow_upward,
-                          size: 40,
-                          color: Colors.red,
-                        ),
-                      ])
-                    ],
-                  ),
-                ],
-              ),
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-                Row(children: <Widget>[
-                  Card(
-                    elevation: 2,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(4)),
-                      child: Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Partially-reflected-plutonium-sphere.jpeg/300px-Partially-reflected-plutonium-sphere.jpeg", width: 80, height: 70),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text('Demon Core', textAlign: TextAlign.left, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                      RaisedButton(
-                          child: Text('Stok 1'),
-                          color: Colors.red,
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(16.0))),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => TransactionDetail()),
-                            );
-                          }),
-                      Row(children: <Widget>[
-                        Icon(
-                          Icons.arrow_upward,
-                          size: 40,
-                          color: Colors.red,
-                        ),
-                      ])
-                    ],
-                  ),
-                ])
-              ]),
-              Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-                Row(children: <Widget>[
-                  Card(
-                    elevation: 2,
-                    child: ClipRRect(
-                      borderRadius: BorderRadius.all(Radius.circular(4)),
-                      child: Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Partially-reflected-plutonium-sphere.jpeg/300px-Partially-reflected-plutonium-sphere.jpeg", width: 80, height: 70),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Text(
-                        'Demon Core',
-                        textAlign: TextAlign.left,
-                        style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Demon Core', textAlign: TextAlign.left, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                    RaisedButton(
+                        child: Text('Stok 1'),
+                        color: Colors.red,
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(16.0))),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => TransactionDetail()),
+                          );
+                        }),
+                    Row(children: <Widget>[
+                      Icon(
+                        Icons.arrow_upward,
+                        size: 40,
+                        color: Colors.red,
                       ),
-                      RaisedButton(
-                          child: Text('Stok 1'),
-                          color: Colors.red,
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(16.0))),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => TransactionDetail()),
-                            );
-                          }),
-                      Row(children: <Widget>[
-                        Icon(
-                          Icons.arrow_upward,
-                          size: 40,
-                          color: Colors.red,
+                    ])
+                  ],
+                ),
+              ],
+            ),
+            Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+              Row(children: <Widget>[
+                Card(
+                  elevation: 2,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(4)),
+                    child: Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Partially-reflected-plutonium-sphere.jpeg/300px-Partially-reflected-plutonium-sphere.jpeg", width: 80, height: 70),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text('Demon Core', textAlign: TextAlign.left, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                    RaisedButton(
+                        child: Text('Stok 1'),
+                        color: Colors.red,
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(16.0))),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => TransactionDetail()),
+                          );
+                        }),
+                    Row(children: <Widget>[
+                      Icon(
+                        Icons.arrow_upward,
+                        size: 40,
+                        color: Colors.red,
+                      ),
+                    ])
+                  ],
+                ),
+              ])
+            ]),
+            Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+              Row(children: <Widget>[
+                Card(
+                  elevation: 2,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.all(Radius.circular(4)),
+                    child: Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Partially-reflected-plutonium-sphere.jpeg/300px-Partially-reflected-plutonium-sphere.jpeg", width: 80, height: 70),
+                  ),
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'Demon Core',
+                      textAlign: TextAlign.left,
+                      style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                    RaisedButton(
+                        child: Text('Stok 1'),
+                        color: Colors.red,
+                        shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(16.0))),
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(builder: (context) => TransactionDetail()),
+                          );
+                        }),
+                    Row(children: <Widget>[
+                      Icon(
+                        Icons.arrow_upward,
+                        size: 40,
+                        color: Colors.red,
+                      ),
+                    ]),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Demon Core', textAlign: TextAlign.left, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        RaisedButton(
+                            child: Text('Stok 1'),
+                            color: Colors.red,
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(16.0))),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => TransactionDetail()),
+                              );
+                            }),
+                        Row(children: <Widget>[
+                          Icon(
+                            Icons.arrow_upward,
+                            size: 40,
+                            color: Colors.red,
+                          ),
+                        ])
+                      ],
+                    ),
+                  ],
+                ),
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+                  Row(children: <Widget>[
+                    Card(
+                      elevation: 2,
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.all(Radius.circular(4)),
+                        child: Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Partially-reflected-plutonium-sphere.jpeg/300px-Partially-reflected-plutonium-sphere.jpeg", width: 80, height: 70),
+                      ),
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text('Demon Core', textAlign: TextAlign.left, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+                        RaisedButton(
+                            child: Text('Stok 1'),
+                            color: Colors.red,
+                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(16.0))),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(builder: (context) => TransactionDetail()),
+                              );
+                            }),
+                        Row(children: <Widget>[
+                          Icon(
+                            Icons.arrow_upward,
+                            size: 40,
+                            color: Colors.red,
+                          ),
+                        ])
+                      ],
+                    ),
+                  ])
+                ]),
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Card(
+                        elevation: 2,
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.all(Radius.circular(4)),
+                          child: Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Partially-reflected-plutonium-sphere.jpeg/300px-Partially-reflected-plutonium-sphere.jpeg", width: 80, height: 70),
                         ),
-                      ]),
-                      Align(
-                        alignment: Alignment.bottomRight,
-                        child: RaisedButton.icon(
-                          color: Colors.green,
-                          label: Text('New Transaction', style: const TextStyle(fontSize: 15)),
-                          icon: Icon(Icons.add),
-                          shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(32.0))),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => TransactionAddItem()),
-                            );
-                          },
-                        ),
-                      )
+                      ),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            'Demon Core',
+                            textAlign: TextAlign.left,
+                            style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                          ),
+                          RaisedButton(
+                              child: Text('Stok 1'),
+                              color: Colors.red,
+                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(16.0))),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => TransactionDetail()),
+                                );
+                              }),
+                          Row(children: <Widget>[
+                            Icon(
+                              Icons.arrow_upward,
+                              size: 40,
+                              color: Colors.red,
+                            ),
+                          ]),
+                          Align(
+                            alignment: Alignment.bottomRight,
+                            child: RaisedButton.icon(
+                              color: Colors.green,
+                              label: Text('New Transaction', style: const TextStyle(fontSize: 15)),
+                              icon: Icon(Icons.add),
+                              shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(32.0))),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(builder: (context) => TransactionAddItem()),
+                                );
+                              },
+                            ),
+                          )
+                        ],
+                      ),
                     ],
                   ),
                 ]),
               ])
-            ])));
+            ])
+          ]),
+        ));
   }
 }
