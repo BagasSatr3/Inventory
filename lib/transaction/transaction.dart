b3886f1196cfc41cbb067ac9c01dc941aa858514
FlutLab.io <>
2022-05-15 00:34:12 +0000

    Autosave

--- lib/transaction/transaction.dart
+++ lib/transaction/transaction.dart
@@ -141,40 +141,6 @@ class Main extends StatelessWidget {
                             size: 40,
                             color: Colors.red,
                           ),
-                        ])
-                      ],
-                    ),
-                  ],
-                ),
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: <Widget>[
-                  Row(children: <Widget>[
-                    Card(
-                      elevation: 2,
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.all(Radius.circular(4)),
-                        child: Image.network("https://upload.wikimedia.org/wikipedia/commons/thumb/1/13/Partially-reflected-plutonium-sphere.jpeg/300px-Partially-reflected-plutonium-sphere.jpeg", width: 80, height: 70),
-                      ),
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Text('Demon Core', textAlign: TextAlign.left, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
-                        RaisedButton(
-                            child: Text('Stok 1'),
-                            color: Colors.red,
-                            shape: RoundedRectangleBorder(borderRadius: BorderRadius.all(Radius.circular(16.0))),
-                            onPressed: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(builder: (context) => TransactionDetail()),
-                              );
-                            }),
-                        Row(children: <Widget>[
-                          Icon(
-                            Icons.arrow_upward,
-                            size: 40,
-                            color: Colors.red,
-                          ),
                         ]),
                         Align(
                           alignment: Alignment.bottomRight,
@@ -193,8 +159,8 @@ class Main extends StatelessWidget {
                         )
                       ],
                     ),
-                  ])
-                ]),
+                  ],
+                ),
               ])
             ])
           ]),
